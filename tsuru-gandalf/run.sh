a29be263824d21af11bd01de940cab7d0e13b01c
thiagonego <thiagonego@gmail.com>
2016-08-22 12:04:11 -0300

    tsuru-gandaf começando a funcionar

--- tsuru-gandalf/run.sh
+++ tsuru-gandalf/run.sh
@@ -1,2 +1,8 @@
-#su - git -c "/usr/bin/gandalf-server -config=/etc/gandalf.conf"
+#!/bin/bash
+
+mkdir -p /data/gandalf/ssh
+chown -R git:git /data/gandalf
+
+service ssh start
+/usr/sbin/rsyslogd
 /usr/bin/gandalf-server -config=/etc/gandalf.conf
